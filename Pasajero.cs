a4d4fd3063be6cd60baa9f1d86cc094d05c9aa5b
jhanjhun <jhanlucassolanosantos@gmail.com>
2025-04-29 20:02:21 -0500

    Update Pasajero.cs

--- Pasajero.cs
+++ Pasajero.cs
@@ -1,25 +1,3 @@
-/*
-public class Pasajero
-    {
-        public string CategoriaPasajero { get; set; }
-        public string PersonaContacto { get; set; }
-        public string TelefonoContacto { get; set; }
-        public List<Boleto> BoletosComprados { get; set; }
-
-        public Pasajero(string categoriaPasajero, string personaContacto, string telefonoContacto)
-        {
-            CategoriaPasajero = categoriaPasajero;
-            PersonaContacto = personaContacto;
-            TelefonoContacto = telefonoContacto;
-            BoletosComprados = new List<Boleto>();
-        }
-
-        public bool ComprarBoleto(Boleto boleto)
-        {
-            BoletosComprados.Add(boleto);
-            return true;
-        }
-    }/*/
 
     public class Pasajero
     {
